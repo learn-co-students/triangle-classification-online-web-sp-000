4087a2b032057030b7d8698a48939261006ddefc
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-26 22:24:45 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :x, :y, :z
+
+  def initialize(x, y, z)
+    @x, @y, @z = x, y, z
+  end
+
+  def kind
+    raise TriangleError unless valid_triangle?
+    if equilateral?
+      :equilateral
+    elsif scalene?
+      :scalene
+    else
+      :isosceles
+    end
+  end
+
+  def valid_triangle?
+    return true if ((x.positive? && y.positive? && z.positive?) && (x + y > z && z + y > x && z + x > y))
+    return false
+  end
+
+  #types of triangles
+
+  def equilateral?
+    return true if ((self.x == self.y) && (self.y == self.z) && (self.x == self.z))
+    return false
+  end
+
+  def scalene?
+    return true if ((self.x != self.y) && (self.y != self.z) && (self.x != self.z))
+    return false
+  end
+
+
+#ERROR SECTION
+  class TriangleError < StandardError
+    # triangle error code
+  end
+
 end
