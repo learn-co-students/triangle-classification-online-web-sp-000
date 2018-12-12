b720fc1a8a88ed959f16471f43441f0ede7c2f57
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-12-12 16:51:41 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if side1 == 0 || side2 == 0 || side3 == 0
+      raise TriangleError
+    elsif side1 < 0 || side2 < 0 || side3 < 0
+      raise TriangleError
+    elsif side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1
+      raise TriangleError
+    else
+      if side1 == side2 && side2 == side3
+        :equilateral
+      elsif side1 == side2 || side2 == side3 || side1 == side3
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+
+#    def message1
+#      "A triangle's side cannot be negative or nul"
+#    end
+
+#    def message2
+#      "A triangle necessarily has three sides"
+#    end
+
+#    def message3
+#      "Violating triangle inequality is illegal"
+#    end
+
+  end
+
 end
