b52f4d90145c1bad64d65b87d75314a51a5c3b9b
Meisuleen Kased <meisuleen@gmail.com>
2018-12-28 20:32:58 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,19 +1,29 @@
+require 'pry'
 class Triangle
 
   def initialize(a, b, c)
     @triangle_sides = []
-    @triangle_sides << a
-    @triangle_sides << b
-    @triangle_sides << c
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def valid? #validate if it's a triangle
+    #if triangle_sides = (0, 0, 0) => raise TriangleError
+    #if triangle_sides = (3, 5, -2) => raise TriangleError
+    #if triangle_sides = (certain inequalities) => TriangleError
   end
 
   def kind#check for the the side measurements and then return its type as a string
-    if (a == b) && (a == c)
+    if (@a == @b) && (@a == @c)
       :equilateral
-    elsif (a == b) || (a == c) || (b == c)
+    elsif (@a == @b) || (@a == @c) || (@b == @c)
       :isosceles
     else
       :scalene
     end
+  end
+  class TriangleError < StandardError
+
   end
 end
