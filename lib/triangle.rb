2a81622b1fbfd4d7eb543117d7f40f4502ba75eb
Meisuleen Kased <meisuleen@gmail.com>
2018-12-28 04:42:57 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
-  # write code here
+
+  def initialize(a, b, c)
+    @triangle_sides = []
+    @triangle_sides << a
+    @triangle_sides << b
+    @triangle_sides << c
+  end
+
+  def kind#check for the the side measurements and then return its type as a string
+    if (a == b) && (a == c)
+      :equilateral
+    elsif (a == b) || (a == c) || (b == c)
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
