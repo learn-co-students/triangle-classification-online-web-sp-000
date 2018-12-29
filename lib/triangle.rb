4a5faaa87ac7591a438f5dfafd6f98c73affee9a
Meisuleen Kased <meisuleen@gmail.com>
2018-12-29 03:17:04 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,13 +8,22 @@ class Triangle
     @c = c
   end
 
-  def valid? #validate if it's a triangle
-    #if triangle_sides = (0, 0, 0) => raise TriangleError
-    #if triangle_sides = (3, 5, -2) => raise TriangleError
-    #if triangle_sides = (certain inequalities) => TriangleError
+  def triangle_one
+    @a.positive? && @b.positive? && @c.positive?
   end
 
+  def triangle_two
+    @a + @b > @c && @a + @c > @b && @b + @c > @a
+  end
+
+
   def kind#check for the the side measurements and then return its type as a string
+    if triangle_one == true && triangle_two == true
+      puts true
+    else
+      raise TriangleError
+    end
+
     if (@a == @b) && (@a == @c)
       :equilateral
     elsif (@a == @b) || (@a == @c) || (@b == @c)
