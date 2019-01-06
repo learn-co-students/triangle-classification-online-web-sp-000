dc8e6351f9b585ea6998c8735c1df807d3edeb31
Brendan O'Neill <oneill.brendan88@gmail.com>
2019-01-06 16:45:29 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,53 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  
+  attr_accessor :side_a, :side_b, :side_c, :kind 
+  
+  
+ class TriangleError < StandardError
+  end
+  
+  
+  def initialize(side_a,side_b,side_c)
+    @side_a = side_a 
+    @side_b = side_b 
+    @side_c = side_c 
+    
+    if side_a == 0 && side_b == 0 && side_c == 0 
+     @kind = :invalid
+    
+    elsif side_a < 0 || side_b < 0 || side_c < 0 
+      @kind = :invalid
+      
+    elsif side_a >= (side_b + side_c) || side_b >= (side_c + side_a) || side_c >= (side_a + side_b)
+      @kind = :invalid 
+    
+    elsif side_a == side_b && side_b == side_c && side_a != 0 && side_b != 0 && side_c != 0
+      @kind = :equilateral 
+    
+    elsif side_a == side_b || side_b == side_c || side_c == side_a 
+      @kind = :isosceles
+      
+    elsif side_a != side_b && side_b != side_c && side_c != side_a 
+      @kind = :scalene
+  
+
+  end
 end
+
+ def kind
+    if @kind == :invalid
+      raise TriangleError 
+    else 
+    @kind 
+  end 
+end
+  
+
+
+end
+
+test = Triangle.new(0,0,0)
+
+
