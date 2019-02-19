80bc1ea2fb8f4aeab2b4d771ce92ebc99e99139d
Indre Bakeer <indre.bakeer@gmail.com>
2019-02-19 00:58:39 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def legal
+    if self.a == self.b && self.a == self.c
+      :equilateral
+    elsif self.a == self.b || self.a == self.c || self.b == self.c
+      :isosceles
+    else self.a != b && self.a != self.c
+      :scalene
+    end
+  end
+
+  def kind
+    if self.a == 0
+      raise TriangleError
+    elsif self.a + self.b <= self.c || self.a + self.c <= self.b || self.b + self.c <= self.a
+      raise TriangleError
+    else
+      legal
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
