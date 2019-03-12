f6ac35e6497cbb5e049360dc1e2900349f937301
Bry Cox <carl.cc@gmail.com>
2019-03-12 14:22:46 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
+require 'pry'
 class Triangle
-  # write code here
+
+  attr_reader :s1, :s2, :s3
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    if (self.s1 + self.s2 <= self.s3) || (self.s2 + self.s3 <= self.s1) || (self.s3 + self.s1 <= self.s2)
+      raise TriangleError
+    elsif self.s1 == self.s2 && self.s2 == self.s3 && self.s3 == self.s1
+      :equilateral
+    elsif self.s1 == self.s2 || self.s2 == self.s3 || self.s3 == self.s1
+      :isosceles
+    elsif
+      self.s1 != self.s2 && self.s2 != self.s3 && self.s3 != self.s1
+      :scalene
+      end
+    end
+
+  class TriangleError < StandardError
+  end
 end
