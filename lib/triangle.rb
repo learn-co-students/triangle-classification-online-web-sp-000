141c82b421a6c375ecc4a6044b7449b55b96a443
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-04-17 19:55:40 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_reader :x, :y, :z
+
+    def initialize(x,y,z)
+      @x = x
+      @y = y
+      @z = z
+    end
+
+    def kind
+        validate
+        if x == y && y == z
+          :equilateral
+        elsif x == y || y == z || z == x
+          :isosceles
+        else
+          :scalene
+        end
+    end
+
+    def validate
+      if (x+y <= z || x+z <= y || y+z <= x)
+        raise TriangleError
+      elsif  (x <= 0 || y <= 0 || z <= 0)
+        raise TriangleError
+      end
+    end
+
+#more elegant
+    #def validate_triangle
+    #real_triangle = [(x + y > z), (x + z > y), (y + z > x)]
+    #[x, y, z].each { |s| real_triangle << false if s <= 0 }
+    #raise TriangleError if real_triangle.include?(false)
+  #end
+
+    class TriangleError < StandardError
+    end
 end
