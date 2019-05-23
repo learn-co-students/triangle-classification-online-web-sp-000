7b08db9e8adc75af741e3c554f85344f23314f53
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-05-23 03:09:21 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize(side1, side2, side3)
+   @side1 = side1
+   @side2 = side2
+   @side3 = side3
+  end
+  
+  def kind
+    #if side1 < 0 
+    
+    if side1 == side2 && side2 == side3
+     kind = :equilateral
+   
+   #elsif
+     #side1 == side2 || side1 == side3 || side2 == #side3
+     #triangle.isosceles
+   #elsif
+   # begin 
+    #raise TriangleError
+    #rescue TriangleError
+    #end
+  end
 end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid Triangle"
+    end
+  end
+end
+
+
