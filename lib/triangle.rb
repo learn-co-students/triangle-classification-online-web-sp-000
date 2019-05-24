d0dcf7a90cb0a4126e29be5d8e2445eb4f9af6f2
Andrew Silva <andrewsilva9@gmail.com>
2019-05-24 02:23:42 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+
+  def initialize(length_one, length_two, length_three)
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
+  end
+
+  def kind
+    if (@length_one <= 0) || (@length_two <= 0) || (@length_three <= 0)
+      raise TriangleError
+    elsif (@length_one+@length_two <= @length_three) || (@length_one+@length_three <= @length_two) || (@length_two+@length_three <= @length_one)
+      raise TriangleError
+    else
+      if (@length_one == @length_two) && (@length_two == @length_three)
+        :equilateral
+      elsif (@length_one == @length_two) || (@length_one == @length_three) || (@length_two == @length_three)
+        :isosceles
+      else (@length_one != @length_two) && (@length_one != @length_three) && (@length_two != @length_three)
+        :scalene
+      end
+    end
+  end
+  class TriangleError < StandardError
+  end
 end
