ade54db2c8e520c156a860f6b5ac3f125e5fe88b
Alan Krajina <alankrajina88@gmail.com>
2019-06-02 13:19:58 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  attr_accessor :length1, :length2, :length3
+  
+  
+  def initialize(length1,length2,length3)
+  @length1 = length1
+  @length2 = length2
+  @length3 = length3
+  end
+  
+  def kind
+  
+  # sorts by size and assigns to new variables
+  a, b, c = [@length1, @length2, @length3].sort
+  
+  # raises ERROR 
+  raise TriangleError if a <= 0 || a + b <= c
+  [nil, :equilateral, :isosceles, :scalene][[a, b, c].uniq.size] 
+
+  end
+  
+  class TriangleError < StandardError
+  end
+  
 end
