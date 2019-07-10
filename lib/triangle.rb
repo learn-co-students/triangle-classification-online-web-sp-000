5771c15c9862013a0ff3765af9e95a8bf5bf2702
Connor Cowan <ccowan003@comcast.net>
2019-07-10 23:59:56 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,13 @@
+# lib/triangle.rb
+
 class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
 
   def initialize(:equilateral, :isosceles, :scalene)
-    
+
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
   end
 end
