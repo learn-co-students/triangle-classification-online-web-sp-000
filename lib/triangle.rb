06e85c77d6d0b82a6f3ecabe698c12adafe2213d
Connor Cowan <ccowan003@comcast.net>
2019-07-11 01:17:06 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,30 @@
-# lib/triangle.rb
-
 class Triangle
-  attr_accessor :equilateral, :isosceles, :scalene
+  attr_accessor :a, :b, :c        #Side a ,b ,c
 
-  def initialize(:equilateral, :isosceles, :scalene)
-
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
-  class TriangleError < StandardError
-    # triangle error code
+  def kind        #instance method, returns, as a symbol, its type. The valid types are:
+    triangle_numbers      #call method
+    if a == b && b == c
+      :equilateral                    #symbol
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def triangle_numbers      #knows that invalid triangles are illegal
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]        #array of all triangle combinations
+    [a, b, c].each do |side|
+     real_triangle << false if side <= 0                        #checks if triangle is valid
+   raise TriangleError if real_triangle.include?(false)
+   end
+ end
+  class TriangleError < StandardError     #inherited from StandardError
   end
 end
