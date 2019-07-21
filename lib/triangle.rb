3c54955b7098ee65b6e1df33e2d0189be9731912
Lindsey Wells <lindsey.wells84@gmail.com>
2019-07-21 12:03:36 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+require "pry"
 class Triangle
-  # write code here
+  
+  attr_accessor :length_1, :length_2, :length_3
+
+def initialize(length_1, length_2, length_3)
+  @length_1 = length_1
+  @length_2 = length_2
+  @length_3 = length_3
+end
+
+class TriangleError < StandardError
+end
+
+def illegal?
+  length_array = [@length_1, @length_2, @length_3]
+  if length_array.any? {|length| length == nil || length <= 0}
+    "illegal"
+    elsif
+    @length_1 + @length_2 <= @length_3 || @length_2 + @length_3 <= @length_1 || @length_1 + @length_3 <= @length_2
+    "illegal"
+  else
+    "legal"
+end
+end
+
+def kind
+  if illegal? == "illegal"
+    raise TriangleError
+elsif @length_1 == @length_2 && @length_2 == @length_3
+  :equilateral
+elsif
+@length_1 == @length_2 || @length_2 == @length_3 || @length_1 == @length_3
+:isosceles
+else
+:scalene
+end
+end
+
 end
