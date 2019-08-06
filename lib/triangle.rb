bc870148bc50945af7650a071e309b29fcf6ab52
Chris Cheong <chrisxcheong@gmail.com>
2019-08-06 06:04:54 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,58 +1,31 @@
-class TriangleError < StandardError
-  def message
-    "This is an invalid triangle"
-  end
-end
-
 class Triangle
-  # write code here
-  attr_accessor :first, :second, :third
-
-  def initialize(first, second, third)
-    @first = first
-    @second = second
-    @third = third
-  end
-
-  def sides
-    [@first, @second, @third]
-  end
-
-  def valid?
-    if self.sides.any? { |e| e == 0 }
-      true
-    elsif self.sides.any? { |e| e < 0  }
-      true
-    elsif @first + @second <= @third
-      true
-    elsif @second + @third <= @first
-      true
-    elsif @third + @first <= @second
-      true
-    else
-      false
-    end
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
-    if self.sides.any? { |e| e_is_a?(numeric)}
-      else raise TriangleError
-    if self.sides.any? { |e| e == 0 }
-        raise TriangleError
-      elsif self.sides.any? { |e| e < 0  }
-        raise TriangleError
-      elsif @first + @second >= @third
-        raise TriangleError
-      elsif @second + @third >= @first
-        raise TriangleError
-      elsif @third + @first >= @second
-        raise TriangleError
-      elsif self.sides.all? { |e| e == @first }
-        :equilateral
-      elsif self.sides.any? {|e| self.sides.count(e) == 2 }
-        :isosceles
-      else
-        :scalene
-      end
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
     end
+  end
+
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      real_triangle << false if side <= 0
+    raise TriangleError if real_triangle.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
