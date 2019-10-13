9372d052036e37be471fc2d534545f6812468bb5
Blythe Pollard <blytheepollard@gmail.com>
2019-10-13 23:55:57 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,7 +19,7 @@ class Triangle
   end
   
   def validate_triangle
-    if ((side_1 + side_2 < side_3) || (side_2 + side_3 < side_1) || (side_1 + side_3 < side_2) || 
+    if ((side_1 + side_2 <= side_3) || (side_2 + side_3 <= side_1) || (side_1 + side_3 <= side_2) || 
       (side_1 <= 0 || side_2 <= 0 || side_3 <= 0))
       raise TriangleError 
     # elsif   
