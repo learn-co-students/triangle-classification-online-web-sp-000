c547c8766d371995e9a020e06c95e61e474f4397
Anthony Tilelli <anthony@tilelli.me>
2019-10-25 19:54:30 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
+require "pry"
 class Triangle
-  # write code here
+  # triangle code
+  def initialize(sideA,sideB,sideC)
+    @sideA = sideA 
+    @sideB = sideB
+    @sideC = sideC
+  end
+ 
+  def kind
+    validate_triangle
+    if @sideA == @sideB && @sideB == @sideC
+      :equilateral
+    elsif @sideA == @sideB || @sideA == @sideC || @sideB == @sideC
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  def validate_triangle
+    if @sideA <= 0
+      raise TriangleError.new("SideA is #{@SideA}")
+    end
+    if @sideB <= 0
+      raise TriangleError.new("SideB is #{@SideB}")
+    end
+    if @sideC <= 0
+      raise TriangleError.new("SideA is #{@SideC}")
+    end
+    # triangle inequality
+    raise TriangleError.new("Invalid Size") if @sideA + @sideB <= @sideC
+    raise TriangleError.new("Invalid Size") if @sideA + @sideC <= @sideB
+    raise TriangleError.new("Invalid Size") if @sideB + @sideC <= @sideA
+  end
+ 
+  class TriangleError < StandardError
+  end
 end
