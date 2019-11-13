26386cd8ba38c62a0894df61e186f65482c3999a
Michael Carroll <carrollm2@gmail.com>
2019-11-13 02:20:43 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,21 +11,27 @@ class Triangle
   def kind
     length_array = [length1, length2, length3]
 
-    sides_length_test = length_array.all?(|side| side > 0)
-    sides_sum = length_array.sum
+    sides_length_test = length_array.all?{|side| side > 0}
 
-    length_array.each_with_index do |val, index|
-      
-
+    sides_sum_test = true
+    if length1 + length2 <= length3
+      sides_sum_test = false
+    elsif length1 + length3 <= length2
+      sides_sum_test = false
+    elsif length2 + length3 <= length1
+      sides_sum_test = false
     end
 
-    #if length1 == length2 && length2 == length3
-    if length_array.uniq.length == 1
-      :equilateral
-    elsif length_array.uniq.length == 2
-      :isosceles
-    elsif length_array.uniq.length == 3
-      :scalene
+    if sides_length_test == false || sides_sum_test == false
+      raise TriangleError
+    else
+      if length_array.uniq.length == 1
+        :equilateral
+      elsif length_array.uniq.length == 2
+        :isosceles
+      elsif length_array.uniq.length == 3
+        :scalene
+      end
     end
   end
 
