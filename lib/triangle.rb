0728b9f8db757fe56a782c1f715cfb00b492d024
Michael Carroll <carrollm2@gmail.com>
2019-11-13 01:07:06 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,28 @@ class Triangle
   end
 
   def kind
-    if length1 == length2 && length2 == length3
-      :equilateral
+    length_array = [length1, length2, length3]
+
+    sides_length_test = length_array.all?(|side| side > 0)
+    sides_sum = length_array.sum
+
+    length_array.each_with_index do |val, index|
+      
+
     end
+
+    #if length1 == length2 && length2 == length3
+    if length_array.uniq.length == 1
+      :equilateral
+    elsif length_array.uniq.length == 2
+      :isosceles
+    elsif length_array.uniq.length == 3
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+
   end
 
 end
