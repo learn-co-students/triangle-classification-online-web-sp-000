c1775e7a34037b7f148546d22ffd5254066a44e8
Winston Pemberton <winstonpemberton@gmail.com>
2019-12-05 00:05:41 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,47 @@
+require 'pry'
 class Triangle
   # write code here
+
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+
+    if side_1 <= 0 || side_2 <=0 || side_3 <= 0
+      #begin
+        raise TriangleError
+      # rescue TriangleError => error
+      #   puts error.message
+      # end
+
+    elsif side_1 + side_2 <= side_3 || side_1 + side_3 <= side_2 || side_2 + side_3 <= side_1
+      raise TriangleError
+
+    else
+      if side_1 == side_2 && side_1 == side_3 && side_2 == side_3
+        :equilateral
+      elsif side_1 == side_2 || side_1 == side_3 || side_2 == side_3
+        :isosceles
+        #binding.pry
+      elsif side_1 != side_2 && side_1 != side_3 && side_2 != side_3
+        :scalene
+      #  binding.pry
+    end
+
+    end
+
+  end
+  class TriangleError < StandardError
+
+    # def message
+    #   puts "Incorrect side measurement passed"
+    # end
+
+  end
+
 end
