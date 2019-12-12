501b02326198d6c9fbe876eebdb26b7983fb043b
Bruce Ruffin <bruffin1221@gmail.com>
2019-12-12 04:17:22 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
   # write code here
 end
+
+
+a=5
+b=3
+c=4
+
+
+a+b<c && a+c<b && b+c<a
+
+case
+when a!=b && b!=c && a!=c
+  "isosceles"
+when 
+when a==b && a==c && b==c
+  "equilateral"
+end
