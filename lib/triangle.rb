4e731aa4fbed293549b3016a3ead6616fe8cf2e7
Bruce Ruffin <bruffin1221@gmail.com>
2019-12-14 21:41:36 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,19 +1,53 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+attr_accessor :a, :b, :c
+
+def initialize(a, b, c)
+  @a=a
+  @b=b
+  @c=c
 end
 
-
-a=5
-b=3
-c=4
-
-
-a+b<c && a+c<b && b+c<a
-
-case
-when a!=b && b!=c && a!=c
-  "isosceles"
-when 
-when a==b && a==c && b==c
-  "equilateral"
+def kind
+  triangle=a+c>b && b+c>a && a+b>c && a>0 && b>0 && c>0
+  if triangle==true && a==b && b==c && c==a
+    :equilateral
+  elsif triangle==true && b==c && a!=b && a!=c
+    :isosceles
+  elsif triangle==true && a==c && b!=c && b!=a
+    :isosceles
+  elsif triangle==true && a==b && b!=c && a!=c
+    :isosceles
+  elsif triangle==true && a!=b && a!=c && c!=b
+    :scalene
+  else triangle!=true
+    raise TriangleError
+  end
 end
+
+  class TriangleError<StandardError
+
+  end
+
+end
+
+#def kind(a,b,c)
+#  triangle=a+c>b && b+c>a && a+b>c && a>0 && b>0 && c>0
+#  if triangle==true && a==b && b==c && c==a
+#    :equilateral
+#  elsif triangle==true && b==c && a!=b && a!=c
+#    :isosceles
+#  elsif triangle==true && a==c && b!=c && b!=a
+#    :isosceles
+  #elsif triangle==true && a==b && b!=c && a!=c
+  #  :isosceles
+  #elsif triangle==true && a!=b && a!=c && c!=b
+  #  :scalene
+  #else triangle!=true
+  #  :invalid
+  #end
+#end
+
+#kind(2,4,2)
