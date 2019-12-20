b0f213ee8579fabdfb82691ff491f55080172a84
Jason J Cruz <jaycru06@yahoo.com>
2019-12-20 05:40:02 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,6 @@
+# The sum of the lengths of any two sides of a triangle always exceeds the length of the third side. This is a principle known as the triangle inequality.
+# Further, each side must be larger than 0.
+
 class Triangle
   attr_accessor :side_1, :side_2, :side_3, :kind 
   
@@ -7,17 +10,25 @@ class Triangle
     @side_3 = side_3 
   end 
   
-  # The sum of the lengths of any two sides of a triangle always exceeds the length of the third side. This is a principle known as the triangle inequality.
-
-  # Further, each side must be larger than 0.
+  def triangle_valid_error 
+    (side_1 + side_2) <= side_3 || (side_1 + side_3) <= side_2 || 
+    (side_3 + side_2) <= side_1 || (side_1 * side_2 * side_3) == 0
+  end 
+  
+  def tri_equilateral
+    side_1 == side_2 && side_1 == side_3
+  end 
+  
+  def tri_isosceles 
+    side_1 == side_2 ||  side_1 == side_3 || side_2 == side_3
+  end 
   
   def kind
-    if (side_1 + side_2) <= side_3 || (side_1 + side_3) <= side_2 || 
-      (side_3 + side_2) <= side_1 || (side_1 * side_2 * side_3) == 0
+    if triangle_valid_error 
       raise TriangleError
-    elsif side_1 == side_2 && side_1 == side_3
+    elsif tri_equilateral 
       "equilateral".to_sym 
-    elsif side_1 == side_2 ||  side_1 == side_3 || side_2 == side_3 
+    elsif tri_isosceles 
       "isosceles".to_sym 
     else 
       "scalene".to_sym 
