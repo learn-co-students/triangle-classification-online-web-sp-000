c699ecd81a41762d0cfbdbc4642a41c206e2cb29
Will Carter <will.carter@fergusllc.com>
2020-01-17 15:15:21 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side1Length, :side2Length, :side3Length
+  
+  def initialize(side1Length, side2Length, side3Length)
+    @side1Length = side1Length
+    @side2Length = side2Length
+    @side3Length = side3Length
+  end
+
+  def kind
+    
+    if(side1Length == 0 || side2Length == 0 || side3Length == 0)
+      begin
+        raise TriangleError
+      end
+    elsif(side1Length < 0 || side2Length < 0 || side3Length < 0)
+      begin
+        raise TriangleError
+      end
+    elsif(
+          (side1Length + side2Length) <= side3Length ||
+          (side1Length + side3Length) <= side2Length ||
+          (side2Length + side3Length) <= side1Length
+         )
+      begin
+        raise TriangleError
+      end
+    elsif(side1Length == side2Length && side2Length == side3Length)
+      :equilateral
+    elsif(side1Length == side2Length || side1Length == side3Length || side2Length == side3Length)
+      :isosceles
+    elsif(side1Length != side2Length && side2Length != side3Length)
+      :scalene
+    end
+    
+  end
+
+  class TriangleError < StandardError
+    def message
+      "All side lengths must be greater than 0"
+    end
+  end
+
 end
