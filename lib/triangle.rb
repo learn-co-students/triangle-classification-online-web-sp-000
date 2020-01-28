5ba249e1af3b843e4cd395004e305440e80132ce
cantstopcoding <jonathan_koss@yahoo.com>
2020-01-28 11:24:22 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,32 +2,47 @@ require 'pry'
 
 class Triangle
   # write code here
-
+  @tri_sides = []
+  
   def initialize(side_1, side_2, side_3)
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
+    @tri_sides = [@side_1, @side_2, @side_3]
   end
 
   def kind 
-    # Use conditional logic to see if the triangle has equal sides
-    # if 2 sides are equal return :equilateral
-    # How do I represent the sides? 
-    if @side_1 == 0 || @side_2 || 0 || @side_3 == 0
-      raise TriangleError
-    else 
-      if @side_1 == @side_2 && @side_1 == @side_3 && @side_2 == @side_3 
-        :equilateral
-      elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3 
-        :isosceles
-      else
-        :scalene
-      end
+    # What has to happen in order for me to raise a TriangleError?
+    # If illegal then raise Triangle Error
+    
+    if valid? == false || negative? == true
+      raise TriangleError 
+    elsif (@side_1 == @side_2) && (@side_1 == @side_3) && (@side_2 == @side_3) 
+      :equilateral
+    elsif (@side_1 == @side_2) || (@side_1 == @side_3) || (@side_2 == @side_3) 
+      :isosceles
+    elsif (@side_1 != @side_2) && (@side_1 != @side_3) && (@side_2 != @side_3)
+      :scalene
     end
   end
 
-  class TriangleError
+  def valid?
+    (@side_1 + @side_2 > @side_3) && (@side_1 + @side_3 > @side_2) && 
+    (@side_2 + @side_3 > @side_1) 
+  end 
+
+  def negative?
+    @tri_sides.each do |side_length|
+      if side_length <= 0
+        return true
+      end
+    end 
+  end
+
+  class TriangleError < StandardError
 
   end
 
 end
+
+# binding.pry
