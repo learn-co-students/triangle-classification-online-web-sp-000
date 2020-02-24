431e70d59b0cdb4c2bcf158e06f7f7ec904172af
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-24 06:43:56 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
-  # write code here
-end
+  
+  attr_accessor :a, :b, :c
+  
+  def initialize(a,b,c)
+    @a = a 
+    @b = b
+    @c = c
+  end
+  
+  def kind
+    
+    if @a == 0 || @b == 0 || @c == 0
+      raise TriangleError
+      
+    elsif @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
+      raise TriangleError
+      
+    elsif @a == @b && @b == @c
+    :equilateral
+    
+    elsif @a == @b || @b == @c || @c == @a
+    :isosceles
+    
+    else
+    :scalene
+    
+  end
+  
+end 
+  
+
+  
+  class TriangleError < StandardError
+    def message
+     puts "that's not a triangle!"
+    end 
+  end
+  
+end 
+
+    
