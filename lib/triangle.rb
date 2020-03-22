5cc76cb7f0107c89d34753dc4f91f194cb82022f
Alex Romo III <alexromo3rd@gmail.com>
2020-03-22 08:01:57 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_accessor :sides
+
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3].sort
+  end
+
+  def kind
+    if @sides.any? { |x| x <= 0 }
+      raise TriangleError, "Sides must be greater than 0"
+    elsif @sides[0] + @sides[1] <= @sides[2]
+      raise TriangleError, "Not valid triangle lengths"
+    else
+      type
+    end
+  end
+
+  def type
+    case @sides.uniq.size
+      when 1 then :equilateral
+      when 2 then :isosceles
+      else :scalene
+    end
+  end
+  class TriangleError < StandardError
+  end
 end
