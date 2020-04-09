f60629a5bff59e372e2990e30346adbabe025d1f
C. Leow <chienleow1@gmail.com>
2020-04-09 03:47:39 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,15 +11,14 @@ class Triangle
   end
   
   def kind
-
     if s1 == s2 && s2 == s3
       return :equilateral
     elsif s2 == s3 || s1 == s3 || s1 == s2
       return :isosceles
     elsif !(s2 == s3 || s1 == s3 || s1 == s2)
       return :scalene
-    elsif 
-    else
+    elsif (s1||s2||s3) == 0
+    binding.pry
       raise TriangleError
     end
   
