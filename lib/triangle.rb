b5f09989c3ec5b031fcd9bb9a7abf19b2806977e
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-26 03:47:19 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+  
+  def kind
+    if @side_1 == @side_2 && @side_2 == @side_3 && @side_1 == @side_3
+      :equilateral
+    elsif (@side_1 == @side_2 && @side_1 != @side_3 && @side_2 != @side_3) || 
+          (@side_1 == @side_3 && @side_1 != @side_2 && @side_2 != @side_3) ||
+          (@side_3 == @side_2 && @side_1 != @side_3 && @side_1 != @side_2)
+      :isosceles
+    elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
+      :scalene
+    elsif @side_1 == 0 && @side_2 == 0 && @side_3 == 0
+      raise TriangleError
+    elsif @side_1 < 0 || @side_2 < 0 || @side_3 < 0
+      raise TriangleError
+    elsif !(@side_1 + @side_2 > @side_3 && @side_2 + @side_3 > @side_1 && @side_1 + @side_3 > @side_2)
+      raise TriangleError
+    end
+      
+  end
+  
+  class TriangleError < StandardError
+    def message
+      "This is customr error"
+    end
+  end
 end
+
+
