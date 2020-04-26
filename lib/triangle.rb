f7c0b53e52498b1c22b39c7b25f99346f412be2e
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-26 14:25:13 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,32 +9,33 @@ class Triangle
   end
   
   def kind
-    if @side_1 == @side_2 && @side_2 == @side_3 && @side_1 == @side_3 && 
-       !(@side_1 == 0 || @side == 0 || @side_3 == 0)
+    if @side_1 == @side_2 && @side_2 == @side_3 && @side_1 == @side_3 && !(@side_1 == 0 && @side == 0 && @side_3 == 0) && (@side_1 + @side_2 > @side_3 && @side_2 + @side_3 > @side_1 && @side_1 + @side_3 > @side_2) && !(@side_1 < 0 && @side_2 < 0 && @side_3 < 0)
       :equilateral
-    elsif (@side_1 == @side_2 && @side_1 != @side_3 && @side_2 != @side_3) || 
+    elsif ((@side_1 == @side_2 && @side_1 != @side_3 && @side_2 != @side_3) || 
           (@side_1 == @side_3 && @side_1 != @side_2 && @side_2 != @side_3) ||
-          (@side_3 == @side_2 && @side_1 != @side_3 && @side_1 != @side_2)
+          (@side_3 == @side_2 && @side_1 != @side_3 && @side_1 != @side_2)) &&
+                 !(@side_1 == 0 && @side == 0 && @side_3 == 0) &&
+            (@side_1 + @side_2 > @side_3 && 
+            @side_2 + @side_3 > @side_1 && 
+            @side_1 + @side_3 > @side_2) &&
+                !(@side_1 < 0 && @side_2 < 0 && @side_3 < 0)
       :isosceles
-    elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3 
+    elsif (@side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3) &&
+           !(@side_1 == 0 && @side == 0 && @side_3 == 0) &&
+            (@side_1 + @side_2 > @side_3 && 
+            @side_2 + @side_3 > @side_1 && 
+            @side_1 + @side_3 > @side_2) &&
+                !(@side_1 < 0 && @side_2 < 0 && @side_3 < 0)
       :scalene
-    elsif @side_1 == 0 && @side_2 == 0 && @side_3 == 0
-
-    elsif @side_1 < 0 || @side_2 < 0 || @side_3 < 0
-
-    elsif !(@side_1 + @side_2 > @side_3 && @side_2 + @side_3 > @side_1 && @side_1 + @side_3 > @side_2)
-      begin 
-        raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
+    else
+      raise TriangleError
     end
       
   end
   
   class TriangleError < StandardError
     def message
-      "This is custom message"
+      "This is a custom message"
     end
   end
 end
