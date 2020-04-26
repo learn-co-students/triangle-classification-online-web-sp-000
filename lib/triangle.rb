77c8a1bd2fec9f2e620e5a30784b1f906c7abae8
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-26 13:49:43 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,26 +10,18 @@ class Triangle
   
   def kind
     if @side_1 == @side_2 && @side_2 == @side_3 && @side_1 == @side_3 && 
-       @side_1 != 0 && @side != 0 && @side_3 != 0
+       !(@side_1 == 0 || @side == 0 || @side_3 == 0)
       :equilateral
     elsif (@side_1 == @side_2 && @side_1 != @side_3 && @side_2 != @side_3) || 
           (@side_1 == @side_3 && @side_1 != @side_2 && @side_2 != @side_3) ||
           (@side_3 == @side_2 && @side_1 != @side_3 && @side_1 != @side_2)
       :isosceles
-    elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
+    elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3 
       :scalene
     elsif @side_1 == 0 && @side_2 == 0 && @side_3 == 0
-      begin 
-        raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
+
     elsif @side_1 < 0 || @side_2 < 0 || @side_3 < 0
-      begin 
-        raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
+
     elsif !(@side_1 + @side_2 > @side_3 && @side_2 + @side_3 > @side_1 && @side_1 + @side_3 > @side_2)
       begin 
         raise TriangleError
