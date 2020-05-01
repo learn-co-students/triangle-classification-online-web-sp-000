6cab5039defdae5bd14dbf3cadae1369baa51ab6
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-05-01 20:07:56 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+      
+
+  def initialize(a, b, c)
+    @c = c
+    @b = b
+    @a = a
+  end 
+
+  def kind 
+    if a <= 0 || b <= 0 || c <= 0 || a + b <= c || b + c <= a || c + a <= b
+      begin
+        raise TriangleError
+      end
+    elsif a == b && a == c && b == c
+      return :equilateral 
+    elsif (a == b) && (a != c)
+      :isosceles
+    elsif (a == c) && (a != b)
+      :isosceles
+    elsif (b == c) && (b != a)
+      :isosceles
+    elsif (b != c) && (a != c) && (b != a)
+      :scalene
+    else 
+      begin 
+        raise TriangleError 
+      end 
+    end
+  end
+        
+        
+  class TriangleError < StandardError
+    puts "illegal"
+  end
+
+
 end
