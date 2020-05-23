d3bcef3e7b8bdadbe64d2b297873f32195ddebce
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-05-23 02:47:01 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,10 @@
 class Triangle
-  # write code here
+  attr_accessor :left_side, :right_side, :middle, :equilateral, :isosceles, :scalene
+  
+  def initialize(attribute)
+    attribute.each {|key, value| self.send(("#{key}="), value)}
+  end
+  
+  def kind
+  end
 end
