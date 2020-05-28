7110b7dcf2eba7fc8aa10ec1b0c46ee6704224a3
Arodriguez1030 <arodriguez1030@gmail.com>
2020-05-28 06:30:18 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,24 @@
 class Triangle
-  attr_accessor :left_side, :right_side, :middle, :equilateral, :isosceles, :scalene
+  attr_accessor :left, :right, :middle, :kind
   
-  def initialize(attribute)
-    attribute.each {|key, value| self.send(("#{key}="), value)}
+  def initialize(left, right, middle)
+    @left = left
+    @right = right
+    @middle = middle 
   end
   
   def kind
+    if @left + @right <= @middle || @right + @middle <= @left || @middle + @left <= @right || @left * @right * @middle <= 0 
+      raise TriangleError
+    elsif @left == @middle && @middle == @right
+      self.kind = :equilateral
+    elsif @left == @middle || @middle == @right || @right == @left
+      self.kind = :isosceles
+    else
+      self.kind = :scalene
+      end
+    end
+
+  class TriangleError < StandardError
   end
 end
