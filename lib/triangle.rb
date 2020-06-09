19a5e7ce2b8574e823308ca25ed7938ae086b014
Jung Lee <lee.jung.s95@gmail.com>
2020-06-09 03:22:27 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c, :kind
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a <= 0 || b <= 0 || c <= 0 || (a + b) <= c || (a + c) <= b || (b + c) <= a
+      raise TriangleError
+    elsif a == b && b == c && c == a
+      return :equilateral
+    elsif a != b && b != c && c != a
+      return :scalene
+    elsif a == b || b == c || a == c
+      return :isosceles
+
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
+
+
 end
