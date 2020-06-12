9962f56d903da77603f5aafd0525fde8cb4967aa
strinie <hello@katherinesalamat.com>
2020-06-11 21:44:18 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+  
+  def kind
+    raise TriangleError if @side_2 + @side_3 <= @side_1 || @side_1 + @side_3 <= @side_2 || @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0 || @side_1 + @side_2 <= @side_3
+      if @side_1 == @side_2 && @side_2 == @side_3
+        @kind = :equilateral
+      elsif @side_2 == @side_3 || @side_1 == @side_3 || @side_1 == @side_2 
+        @kind = :isosceles
+      elsif @side_1 != @side_3 && @side_1 != @side_2 
+        @kind = :scalene
+      end
+    @kind
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid side dimensions!"
+    end
+  end
 end
