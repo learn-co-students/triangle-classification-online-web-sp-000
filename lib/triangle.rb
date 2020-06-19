be8692240622fdc0ab168baa668447c8bb9a14c1
imperative-genre-4923 <dreama.lin@yale.edu>
2020-06-19 01:36:43 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
   # write code here
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+
+    if @x <= 0 || @y <= 0 || @z <= 0
+      raise TriangleError
+    elsif @x + @y <= @z || @x + @z <= @y || @y +@z <= @x
+      raise TriangleError
+
+    else
+
+      if @x == @y && @y == @z
+        :equilateral
+      elsif @x != @y && @y != @z && @x != @z
+        :scalene
+      else
+        :isosceles
+      end
+
+    end
+
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
