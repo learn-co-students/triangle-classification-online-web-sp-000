3cb3dd6665c81be8366d9d33d8c1bb06fd136392
jrog20 <jaimerogstad@gmail.com>
2020-08-11 14:40:45 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,6 @@
-require 'pry'
-
 class Triangle
 
-  attr_accessor :a, :b, :c#, :equilateral, :isosceles, :scalene
+  attr_accessor :a, :b, :c
 
   def initialize(a, b, c)
     @a = a
@@ -12,8 +10,7 @@ class Triangle
 
   def kind
     if !(a+b>c && a+c>b && b+c>a) || a<=0 || b<=0 || c<=0
-      raise TriangleError < StandardError
-      puts error.message
+      raise TriangleError
     elsif a == b && b == c
       :equilateral
     elsif a != b && a != c && b != c
