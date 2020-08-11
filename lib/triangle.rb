f97beda7fc0347f1cc6e49bff427a67d76512d24
jrog20 <jaimerogstad@gmail.com>
2020-08-11 14:34:15 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c#, :equilateral, :isosceles, :scalene
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if !(a+b>c && a+c>b && b+c>a) || a<=0 || b<=0 || c<=0
+      raise TriangleError < StandardError
+      puts error.message
+    elsif a == b && b == c
+      :equilateral
+    elsif a != b && a != c && b != c
+      :scalene
+    elsif a == b || b == c || a == c
+      :isosceles
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This is not a valid triangle."
+    end
+  end
 end
