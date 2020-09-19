ea71445ddb34c0e7c00fda67e67b03de7dc56349
jasonronalddavis <jdavis@c.ringling.edu>
2020-09-18 22:13:29 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
-class Triangle
-  # write code here
+class Triangle 
+
+  def initialize(a, b, c)
+
+    if a >= (b + c) || b >= (a + c) || c >= (a + b)
+      raise TriangleError
+    end
+      
+    if a <= 0 || b <= 0 || c <= 0
+        raise TriangleError
+      end
+      
+      if a == nil || b == nil || c == nil
+        raise TriangleError
+      end
+
+     
+
+
+      @a = a
+      @b = b
+      @c = c
+    end
+ 
+
+  
+  
+  def kind
+    if @a == @b && @a == @c
+      return :equilateral
+    elsif @a == @b || @b == @c || @c == @a
+      return :isosceles
+    else 
+      return :scalene
+    end
+  end
 end
+        
+
+
+
+class TriangleError < StandardError
+  def message
+    "This is not a valid triangle."
+  end
+end 	
