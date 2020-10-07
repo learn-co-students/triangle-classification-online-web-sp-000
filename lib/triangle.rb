e0a07426f6361be1f0fc0327dd828dde912ded94
John Troutman <jtroutmanman@gmail.com>
2020-10-07 06:06:15 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,9 +20,6 @@ class Triangle
     elsif
       @side1 == @side2 && @side1 != @side3 || @side1 == @side3 && @side1 != @side2 || @side2 == @side3 && @side2 != @side1
       :isosceles
-    elsif
-      @side1 == 0 || @side2 == 0 || @side3 == 0
-      raise TriangleError
     end
   end
   
