cb256674e9fb2c5b1a026151e118946b1e0abd71
Catherine Gloria <catherinegloria@gmail.com>
2020-10-25 16:43:29 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a == b && a == c
+      :equilateral
+    elsif a == b && a != c || a == c && a != b || b == c && b != a
+      :isosceles
+    elsif a != b && b!=c
+      :scalene
+    elsif a < 0 || b < 0 || c < 0 #triangle with sides less than 0 or negative
+      begin
+        raise TriangleError
+      rescue TriangleError #=> error
+      end
+    else (a + b) < c || (a + c) < b || (a + c) < b #sides violating the triangle inequality
+      begin
+        raise TriangleError
+      rescue TriangleError
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
