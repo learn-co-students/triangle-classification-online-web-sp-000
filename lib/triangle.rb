b39c7c5030d5abfc09f197a95d4a771e98b68e42
Kelly Hesterly <kelly.hesterly@gmail.com>
2020-10-28 21:42:56 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,53 @@
+require 'pry'
 class Triangle
   # write code here
+  attr_accessor :side_one, :side_two, :side_three, :sides
+
+  @sides = []
+
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+    @sides = [side_one, side_two, side_three]
+  end
+
+  def kind
+    if (negative? == true) || (valid? == false)
+        raise TriangleError
+      elsif(equilateral?)
+        :equilateral
+      elsif(scalene?)
+        :scalene
+      elsif(isosceles?)
+        :isosceles
+      end
+  end
+
+  def negative?
+    @sides.each do |length|
+      if(length <= 0)
+        return true
+    end
+  end
+end
+
+  def valid?
+    (@side_one + @side_two > @side_three) && (@side_one + @side_three > @side_two) && (@side_two + @side_three > @side_one)
+  end
+
+  def equilateral?
+    (@side_one == @side_two) && (@side_one == @side_three)
+  end
+
+  def isosceles?
+    (@side_one == @side_two) || (@side_one == @side_three) || (@side_two == @side_three)
+  end
+
+  def scalene?
+    (@side_one != @side_two) && (@side_one != @side_three) && (@side_two != @side_three)
+  end
+  class TriangleError < StandardError
+
+  end
 end
