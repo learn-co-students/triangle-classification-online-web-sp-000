c570cdedf564df29072c0aa8e0b13b29ac4b1640
Damian Malysza <damian.malysza@gmail.com>
2020-11-04 20:38:04 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+
+  def initialize(l1,l2,l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+    @len_array = [@l1,@l2,@l3]
+  end
+
+  def kind
+    if (@l1 + @l2 <= @l3 || @l2 + @l3 <= @l1 || @l1 + @l3 <= @l2) || @len_array.all? {|l| l <=0}
+      raise TriangleError
+    elsif @len_array.uniq.size == 1
+      :equilateral
+    elsif @len_array.uniq.size == 3
+      :scalene
+    elsif @len_array.uniq.size == 2
+      :isosceles
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
