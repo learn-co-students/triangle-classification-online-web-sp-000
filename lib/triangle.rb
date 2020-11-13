4f0cccba265c8d6da17919f40c83fd07263f049c
fut0k0 <cccabell@icloud.com>
2020-11-12 17:11:21 -0800

    Passing lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,17 +7,15 @@ class Triangle
 	end
 
 	def kind
-		begin
-			raise TriangleError if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
-			raise TriangleError if @side_1 + @side_2 <= @side_3 || @side_1 + @side_3 <= @side_2 || @side_2 + @side_3 <= @side_1
+		raise TriangleError if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
+		raise TriangleError if @side_1 + @side_2 <= @side_3 || @side_1 + @side_3 <= @side_2 || @side_2 + @side_3 <= @side_1
 
-			if @side_1 == @side_2 && @side_1 == @side_3
-				:equilateral
-			elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
-				:isosceles
-			else
-				:scalene
-			end
+		if @side_1 == @side_2 && @side_1 == @side_3
+			:equilateral
+		elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+			:isosceles
+		else
+			:scalene
 		end
 	end
 
