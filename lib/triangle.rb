b296f0bb8a0f0ad4f786df7bf492000a899421a4
Delia Constantino <dconstan92@gmail.com>
2020-11-13 23:51:11 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :side_a, :side_b, :side_c, :all_sides
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+    @all_sides = []
+    self.all_s(@side_a, @side_b, @side_c)
+  end
+
+  def all_s(a, b, c)
+    @all_sides.push(a, b, c)
+  end
+
+  def kind
+    if @all_sides.any? {|num| num <= 0}
+      raise TriangleError
+    elsif @all_sides.any? {|num| num >= @all_sides.sum - num }
+      raise TriangleError
+    elsif @all_sides[0] == @all_sides[1] && @all_sides[1] == @all_sides[2]
+      return :equilateral
+    elsif @all_sides[0] != @all_sides[1] && @all_sides[1] != @all_sides[2] && @all_sides[0] != @all_sides[2]
+      return :scalene
+    else
+      return :isosceles
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
