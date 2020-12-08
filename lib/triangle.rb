74d1d3cc2aa3bb437cbdefe87318ef68a4c056df
Alison Williams <apw5011@gmail.com>
2020-12-08 19:53:13 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end 
+  
+  class TriangleError < StandardError
+    def message
+      puts "Each side of the triangle must be larger than 0."
+    end 
+  end 
+  
+  def kind 
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+      begin 
+        raise TriangleError
+      end 
+    elsif @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2
+      begin
+        raise TriangleError
+      end 
+    elsif @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+      :scalene
+    else 
+      :isosceles
+    end 
+  end
 end
