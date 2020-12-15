5e8882aebe9330e7070002870b5a9cac21bb03fb
Thor Summerall <thor.summerall@gmail.com>
2020-12-15 14:46:28 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    x = @side_1
+    y = @side_2
+    z = @side_3
+
+    if (x+y<=z || x+z<=y || y+z<=x)
+      raise  TriangleError
+
+    # I THINK this code down below spits out the error message correctly? I can't use it obviously because it fails the tests, but here you go.
+    #  begin
+    #    raise TriangleError
+    #  rescue TriangleError => error
+    #    puts error.message
+    #  end
+    end
+    if (x==y && y==z)
+      return :equilateral
+    elsif (x==y || y==z || z==x)
+      return :isosceles
+    else (x!=y || y!=z || z!=x)
+      return :scalene
+    end
+
+  end
+
+  class TriangleError < StandardError
+    def message
+      puts "Fix the error, you FOOL."
+    end
+  end
 end
