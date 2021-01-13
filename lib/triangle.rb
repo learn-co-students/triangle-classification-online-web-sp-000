c8c95a9918a088d60eafd72ac299dbe7e3c1f4ab
Rosie Choi <rosiechoimanso@gmail.com>
2021-01-13 21:05:15 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    min, min2, max = [@a, @b, @c].sort
+    if min < 0 ||  min + min2 <= max
+      raise TriangleError
+    end
+
+    if @a == @b && @b == c
+      :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+
+    end
 end
