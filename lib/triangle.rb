f9929ec9c890a78a76d78446be6a8cfbacc0ee6e
Ben Altman <altmanguitar@gmail.com>
2021-01-29 21:07:04 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    @sides = [a, b, c]
+    @sides.sort!
+  end
+
+  def kind
+    if @sides.any? {|side| side <= 0} || (@sides[0] + @sides[1] <= @sides[2])
+      raise TriangleError
+    elsif @sides.uniq.count == 1
+      :equilateral
+    elsif @sides.uniq.count == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This isn't even a Bizarre Love Triangle"
+    end
+  end
 end
