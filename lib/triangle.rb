b50668b24ce3ceba03d4359f7b5d60b82e286b0a
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-05 17:21:39 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,40 +1,36 @@
-require 'pry'
 class Triangle
-  def initialize(side_1, side_2, side_3)
-    @triangle_sides = []
-    @triangle_sides << side_1
-    @triangle_sides << side_2
-    @triangle_sides << side_3
-  end
-
-  def valid?
-    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
-    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
-    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
-
-    if (@triangle_sides.none? {|side| side <= 0}) &&
-      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
-      return true
-    else
-      return false
-    end
-  end
-
-  def kind
-    if valid?
-      if @triangle_sides.uniq.length == 1
-        return :equilateral
-      elsif @triangle_sides.uniq.length == 2
-        return :isosceles
-      else
-        return :scalene
-      end
-    else
+  # triangle code
+  attr_accessor :ab, :bc, :ca, :type
+  
+  def initialize(ab,bc,ca)
+    @ab = ab
+    @bc = bc
+    @ca = ca
+  end 
+  
+  def kind 
+    if @ab <= 0 || @bc <= 0 || @ca <= 0 || (@ab + @bc) <= @ca ||  (@ab + @ca) <= @bc || (@ca + @bc) <= @ab
       raise TriangleError
-    end
+=begin
+      begin 
+        raise Triangle::TriangleError.new()
+      rescue Triangle::TriangleError => error 
+        puts error.message
+      end
+=end 
+    elsif @ab == @bc && @bc == @ca 
+      @type = :equilateral 
+    elsif @ab == @bc || @bc == @ca || @ab == @ca 
+      @type = :isosceles 
+    else 
+      @type = :scalene 
+    end 
+  end 
+ 
+  class TriangleError < StandardError
+    # triangle error code
+    def message
+      "oops"
+    end 
   end
-end
-
-class TriangleError < StandardError
-
 end
