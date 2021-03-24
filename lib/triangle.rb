72e9611276bd9107f0341e2433501226ec223aee
Clayon Stewart <c.stewart89@gmail.com>
2021-03-24 14:08:10 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+  
+  @@all =[]
+  
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c 
+    @size = [@a,@b,@c].sort
+    self.class.all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def kind
+    
+    if (@a <= 0  || @b <= 0 || @c <= 0) || (@size[0] + @size[1] <= @size[2]) 
+      raise TriangleError
+    end
+    
+    if @a == @b && @b == @c
+      :equilateral
+    elsif (@a == @b && @a != @c) || (@b == @c && @c != @a) || (@c == @a && @a != @b)
+      :isosceles       
+    elsif (@a != @b && @b != @c && @c != @a)
+      :scalene
+    end
+    
+    
+  end
+  
+  
+  class TriangleError < StandardError
+    def message
+      "Sides do not make a triangle"
+    end
+  end
 end
