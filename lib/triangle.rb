ecc0dc72b135e637dd504d50bbfe3cf3d04d50dc
z-cal <icancodethings@gmail.com>
2021-05-24 16:12:29 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side1length, :side2length, :side3length
+
+  def initialize(side1length, side2length, side3length)
+    @side1length = side1length
+    @side2length = side2length
+    @side3length = side3length
+  end
+
+  def kind
+    valid_triangle?(side1length, side2length, side3length)
+    if side1length == side2length && side2length == side3length
+      :equilateral
+    elsif side1length == side2length || side2length == side3length || side3length == side1length
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def valid_triangle?(side1length, side2length, side3length)
+    a, b, c = [@side1length, @side2length, @side3length].sort
+    if a <= 0 || a + b <= c
+        raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
